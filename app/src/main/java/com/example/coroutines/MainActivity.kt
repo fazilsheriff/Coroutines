fbb4b4a0949b5563ca86418dcf15d0633790e7e1
fazil.s@lantrasoft.com <SinCos123>
2020-10-08 10:51:18 +0530

    Coroutines like suspend,coroutine scope and

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -1,22 +1,54 @@
 package com.example.coroutines
-
+/*
+* 1 What is the difference between IO,Default and Main
+* 2 What is Coroutines
+* 3 What are the methods in coroutines
+* */
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RestrictTo
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private  val RESULT_1="RESULT #1"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnClcik.setOnClickListener{
-            fnNetworkCallOn()
+            CoroutineScope(Main).launch {
+                fnNetworkCallOn()
+            }
         }
     }
-
-    private fun fnNetworkCallOn() {
+    private suspend fun getResult1API():String
+    {
+        logThread("getResult1API")
+        delay(1000)
+        return RESULT_1
+    }
+//    debug: ${methodName}: ${Thread.currentThread().name
+    private fun logThread(methodName: String) {
+        println("debug: ${methodName} ${Thread.currentThread().name}")
 
     }
+    private suspend fun fnNetworkCallOn() {
+        var result=getResult1API()
+        withContext(Main){
+            displayThreadValuesOnUI(result)
 
-   
+        }
+    }
+    private fun displayThreadValuesOnUI(result:String) {
 
+        var newText= "\n $result"
+        txtHello.text=newText
+
+    }
 }
