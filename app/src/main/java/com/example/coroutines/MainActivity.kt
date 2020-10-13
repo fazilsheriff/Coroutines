4f1a54d248ecd61fe9c8b15860a4632d280df0ed
fazil.s@lantrasoft.com <SinCos123>
2020-10-13 18:02:31 +0530

    Suspend function

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.coroutineScope
@@ -15,24 +16,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "onCreate: "+Thread.currentThread().name)
+        suspend  fun  fourthMethod(){
+            Log.i(TAG, "fourthMethod: "+Thread.currentThread().name)
+        }
+
         btnClcik.setOnClickListener{
-           CoroutineScope(Main).launch {
+           CoroutineScope(Default).launch {
 
                thirdMethod()
                secondMethod()
+               fourthMethod()
            }
         }
     }
 
      fun firstMethod() {
+         Log.i(TAG, "firstMethod: "+Thread.currentThread().name)
+
          Log.i(TAG, "firstMethod: Is executed")
     }
 
     suspend fun secondMethod(){
+        Log.i(TAG, "secondMethod: "+Thread.currentThread().name)
+
         Log.i(TAG, "secondMethod: Is executed")
     }
 
      suspend fun thirdMethod(){
+         Log.i(TAG, "thirdMethod: "+Thread.currentThread().name)
         Log.i(TAG, "ThridMethod: Is executed")
          firstMethod()
 
