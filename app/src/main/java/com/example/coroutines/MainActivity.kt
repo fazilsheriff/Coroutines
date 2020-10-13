01a7da28eb345a162af7a1f0799957eb11d3755f
fazil.s@lantrasoft.com <SinCos123>
2020-10-13 16:44:03 +0530

    Suspend function

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -2,21 +2,42 @@ package com.example.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private  val TAG = "Coroutines"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnClcik.setOnClickListener{
-            fnNetworkCallOn()
+           CoroutineScope(Main).launch {
+
+               thirdMethod()
+               secondMethod()
+           }
         }
     }
 
-    private fun fnNetworkCallOn() {
-
+     fun firstMethod() {
+         Log.i(TAG, "firstMethod: Is executed")
     }
 
+    suspend fun secondMethod(){
+        Log.i(TAG, "secondMethod: Is executed")
+    }
+
+     suspend fun thirdMethod(){
+        Log.i(TAG, "ThridMethod: Is executed")
+         firstMethod()
+
+
+     }
    
 
 }
