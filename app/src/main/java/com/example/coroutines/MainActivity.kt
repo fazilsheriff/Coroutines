882a5a5f2fdbd2c715a5bdcc92290138ad40e1c3
fazil.s@lantrasoft.com <SinCos123>
2020-10-15 17:14:42 +0530

    Suspend function

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -2,21 +2,29 @@ package com.example.coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
+    private  val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnClcik.setOnClickListener{
-            fnNetworkCallOn()
+
+
+
+            }
         }
     }
 
-    private fun fnNetworkCallOn() {
 
-    }
 
    
 
-}
