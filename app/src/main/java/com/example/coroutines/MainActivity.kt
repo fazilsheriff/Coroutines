508db51effdc601cf046f9049531c392941dde98
fazil.s@lantrasoft.com <SinCos123>
2020-10-29 12:38:24 +0530

    Seqntial ,Paralle and Lazy

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -4,24 +4,44 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
-    private  val TAG = "MainActivity"
+    private  val TAG = "Coroutines"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnClcik.setOnClickListener{
+        Log.i(TAG, "onCreate: Stated the Main Thread, ${Thread.currentThread().name}")
 
+            runBlocking {
+             var timeSec=   measureTimeMillis {
+                    var firstResult:Deferred<String> =async(start = CoroutineStart.LAZY) {
+                        firstMethod()
+                    }
+                 var   secondResult=async {
+                     secondMethod()
+                 }
 
+//                    Log.i(TAG, "onCreate: "+firstResult.await())
+//                    Log.i(TAG, "onCreate: "+secondResult.await())
+                }
+                Log.i(TAG, "Time taken to complete in sec $timeSec")
 
             }
+
+       }
+
+        suspend fun firstMethod(): String {
+            delay(3000L)
+            return "Hello"
         }
+
+    suspend fun secondMethod(): String {
+        delay(3000L)
+        return "World"
+    }
     }
 
 
