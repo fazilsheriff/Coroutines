a87e8581a5596cc4fa9a9532a0913007f75ce28f
fazil.s@lantrasoft.com <SinCos123>
2020-11-10 11:03:08 +0530

    Global scope

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -4,11 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.Main
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +13,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG, "onCreate:Main Start "+Thread.currentThread().name)
+        GlobalScope.launch{
+            delay(5000)
+            Log.i(TAG, "onCreate:GlobalScope  1"+Thread.currentThread().name)
+//
+//            Log.i(TAG, "onCreate: Inside the globalScope")
+//            Log.i(TAG, "onCreate: Hello")
+        }
+        GlobalScope.launch {
+
+            Log.i(TAG, "onCreate:GlobalScope2 "+Thread.currentThread().name)
+            runBlocking {
+                delay(8000)
+                Log.i(TAG, "onCreate:Inside Run Blocking "+Thread.currentThread().name)
+            }
+
+        }
+
+        Log.i(TAG, "onCreate:Main End "+Thread.currentThread().name)
+
         btnClcik.setOnClickListener{
 
 
